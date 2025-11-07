dd18cffd8e9ae0af3cc120fa2f7a0fa1249beb30
Veronika <94937684+aeterraes@users.noreply.github.com>
2025-11-07 10:51:30 +0300

    hw-07

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -139,14 +139,14 @@ class PaymentExternalSystemAdapterImpl(
                                 ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
                             }
 
-                        logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
+                            logger.warn("[$accountName] Payment processed for txId: $transactionId, payment: $paymentId, succeeded: ${body.result}, message: ${body.message}")
 
-                        // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
-                        // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
-                        paymentESService.update(paymentId) {
-                            it.logProcessing(body.result, now(), transactionId, reason = body.message)
-                        }
-                        counter.increment()
+                            // Здесь мы обновляем состояние оплаты в зависимости от результата в базе данных оплат.
+                            // Это требуется сделать ВО ВСЕХ ИСХОДАХ (успешная оплата / неуспешная / ошибочная ситуация)
+                            paymentESService.update(paymentId) {
+                                it.logProcessing(body.result, now(), transactionId, reason = body.message)
+                            }
+                            counter.increment()
 
                             summary.record((now() - startTime) / 1000.0)
 
@@ -179,10 +179,10 @@ class PaymentExternalSystemAdapterImpl(
                         val adder = ThreadLocalRandom.current().nextLong(0, 100)
                         val backoff = (200L * (1L shl (attempt - 1))).coerceAtMost(2000L)
                         val beforeDeadline = deadline - now()
-                        if (beforeDeadline <= 100)  {
+                        if (beforeDeadline <= 60)  {
                             break
                         }
-                        val actualSleep = minOf(backoff + adder, beforeDeadline - 50)
+                        val actualSleep = minOf(backoff + adder, beforeDeadline - 60)
                         if (actualSleep > 0) {
                             try {
                                 Thread.sleep(actualSleep)
